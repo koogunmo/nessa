c7bdc9dab0afa54d728c1e31e2db455011474631
Martin Purcell <martin.purcell@gmail.com>
2014-01-28 14:14:15 +0000

    Angular: Updated to 1.2.10

--- app/assets/js/init.js
+++ app/assets/js/init.js
@@ -2,16 +2,16 @@
 
 requirejs.config({
 	paths: {
-		'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min',
+		'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.10/angular.min',
 		'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min',
-		'ngCookies': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular-cookies.min',
-		'ngResource': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular-resource.min',
-		'ngRoute': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular-route.min',
+		'ngCookies': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.10/angular-cookies.min',
+		'ngResource': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.10/angular-resource.min',
+		'ngRoute': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.10/angular-route.min',
 		'socket.io': 'libs/socket.io.min',
 		'ui.bootstrap': 'libs/ui-bootstrap.min',
 		// For future use
 		'firebase': 'https://cdn.firebase.com/v0/firebase',
-		'angularfire': 'https://cdn.firebase.com/libs/angularfire/0.5.0/angularfire.min'
+		'angularfire': 'https://cdn.firebase.com/libs/angularfire/0.6.0/angularfire.min'
 	},
 	shim: {
 		'angular': {
