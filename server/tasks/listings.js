8945e7c125f3c5390f7704acb6317c1bd8099530
Martin Purcell <martin.purcell@gmail.com>
2014-03-03 19:21:20 +0000

    added try ... catch to listings task

--- server/tasks/listings.js
+++ server/tasks/listings.js
@@ -1,27 +1,31 @@
 /* Update show listings from XML */
-var schedule	= require('node-schedule');
-var shows		= plugin('showdata');
-
-function updateListings(){
-	// Update show listings
-	shows.getShowlist();
+try {
+	var schedule	= require('node-schedule');
+	var shows		= plugin('showdata');
 	
-	// Update show information (synopsis, artwork, etc)
-	var collection = db.collection('show');
-	collection.find({status: {$exists: true}}).toArray(function(error, results){
-		if (error || !results) return;
-		results.forEach(function(show){
-			shows.getArtwork(show.tvdb);
-			shows.getSummary(show.tvdb, function(error, tvdb){
-				shows.getFullListings(tvdb);
+	function updateListings(){
+		// Update show listings
+		shows.getShowlist();
+		
+		// Update show information (synopsis, artwork, etc)
+		var collection = db.collection('show');
+		collection.find({status: {$exists: true}}).toArray(function(error, results){
+			if (error || !results) return;
+			results.forEach(function(show){
+				shows.getArtwork(show.tvdb);
+				shows.getSummary(show.tvdb, function(error, tvdb){
+					shows.getFullListings(tvdb);
+				});
 			});
 		});
-	});
+	}
+	
+	/* Every night at 1am */
+	var rule = new schedule.RecurrenceRule();
+		rule.hour		= 1;
+		rule.minute		= 0;
+	
+	schedule.scheduleJob(rule, updateListings);
+} catch(e){
+	console.error(e.message);
 }
-
-/* Every night at 1am */
-var rule = new schedule.RecurrenceRule();
-	rule.hour		= 1;
-	rule.minute		= 0;
-
-schedule.scheduleJob(rule, updateListings);
