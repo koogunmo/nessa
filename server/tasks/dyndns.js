a67c0d1fa8288dab30c164cf0f5a9af4168daf62
Martin Purcell <martin.purcell@gmail.com>
2014-03-03 14:56:26 +0000

    Preparing for Dynamic DNS update

---
+++ server/tasks/dyndns.js
@@ -0,0 +1,22 @@
+try {
+	var request = require('request'), schedule = require('node-schedule');
+	function dynamicDNS(){
+		request('http://myip.dnsomatic.com', function(error, response, ip){
+			if (error) {
+				console.error(error);
+				return;
+			}
+			request('http://dns.silicomedia.com:3000/api/'+nconf.get('trakt:username')+'/'+ip, function(error, response, json){
+				if (error) console.error(error);
+				console.log(json);
+			});
+		});
+	}
+	var rule = new schedule.RecurrenceRule(); rule.minute = [0];
+	schedule.scheduleJob(rule, function(){
+		dynamicDNS();
+	});
+	dynamicDNS();
+} catch(e){
+	console.error(e.message);
+}
--- server/tasks/upnp.js
+++ server/tasks/upnp.js
@@ -1,18 +1,17 @@
-/*
-var upnp = require('nat-upnp');
-
-var client = upnp.createClient();
-
-var mapPorts = function(){
-	client.portMapping({
-		public: 6377,
-		private: 80,
-		ttl: 60
-	}, function(error, response){
-	//	console.log(error, response);
-	});
-};
-
-setInterval(mapPorts, 60000);
-mapPorts();
-*/
+try {
+	var upnp = require('nat-upnp');
+	var client = upnp.createClient();
+	var mapPorts = function(){
+		client.portMapping({
+			public: 6377,
+			private: 80,
+			ttl: 600
+		}, function(error, response){
+		//	console.log(error, response);
+		});
+	};
+	setInterval(mapPorts, 600000);
+	mapPorts();
+} catch(e){
+	console.error(e.message);
+}
