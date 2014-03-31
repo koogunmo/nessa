607607bf30676769daa9f8cfa9a618044bb100b1
Martin Purcell <martin.purcell@gmail.com>
2014-03-31 10:09:10 +0100

    Preparing for installer

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -73,13 +73,12 @@ define('app', ['angular','socket.io','moment','ngCookies','ngResource','ngTouch'
 	
 	app.factory('$socket', function($rootScope) {
 		var port = '';
-			
 		if (window.location.protocol == 'https:'){
-			port = ':443';
+			port = 443;
 		} else {
 			port = (window.location.port) ? window.location.port : 80;
 		}
-		var socket = io.connect(window.location.protocol+'//'+window.location.hostname+port, {
+		var socket = io.connect(window.location.protocol+'//'+window.location.hostname+':'+port, {
 			'connect timeout': 2000,
 			'max reconnection attempts': 5,
 			'sync disconnect on unload': true
@@ -303,8 +302,12 @@ define('app', ['angular','socket.io','moment','ngCookies','ngResource','ngTouch'
 				window.modal = null;
 			}
 		}).state('install', {
+			url: '/install',
+			controller: 'installCtrl',
+			templateUrl: '/views/partials/install.html',
 			data: {
-				secure: false
+				secure: false,
+				title: 'Install'
 			}	
 		}).state('shows', {
 			url: '/shows',
