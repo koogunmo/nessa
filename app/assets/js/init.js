dd048eb157fd9bd822597a971b13627debe6c599
Martin Purcell <martin.purcell@gmail.com>
2014-06-18 15:23:04 +0100

    Converting glyphicons to FontAwesome

--- app/assets/js/init.js
+++ app/assets/js/init.js
@@ -1,12 +1,12 @@
 'use strict';
 requirejs.config({
 	paths: {
-		'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.11/angular.min',
+		'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.13/angular.min',
 		'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
 		'moment': 'libs/moment.min',
-		'ngResource': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.11/angular-resource.min',
+		'ngResource': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.13/angular-resource.min',
 		'ngStorage': 'libs/ng-storage.min',
-		'ngTouch': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.11/angular-touch.min',
+		'ngTouch': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.13/angular-touch.min',
 		'socket.io': 'libs/socket.io.min',
 		'ui.bootstrap': 'libs/ui-bootstrap.min',
 		'ui.router': 'libs/ui-router.min'
