5150a70052f8514b7f850ccaf2b6e87883d8f4c1
Martin Purcell <martin.purcell@gmail.com>
2015-01-11 17:17:30 +0000

    Auto trash not working

--- server/tasks/torrents.js
+++ server/tasks/torrents.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var log4js = require('log4js');
+log4js.configure({
+	appenders: [{
+		type: 'console'
+	}],
+	replaceConsole: true
+});
+var logger = log4js.getLogger('task-torrents');
+
+
 /* Check for completed downloads every 5 minutes */
 module.exports = function(app,db,socket){
 	var movies	= require('nodetv-movies'),
@@ -8,17 +18,20 @@ module.exports = function(app,db,socket){
 	
 	try {
 		var checkDownloads = function(){
+			logger.debug('Checking downloads...');
 			torrent.getComplete(function(error, transfers){
 				transfers.forEach(function(transfer){
-					if (movies.complete) {
+				//	logger.debug(transfer.title);
+					
+					if (movies.complete){
 						movies.complete(transfer).then(function(data){
-							if (data.trash) torrent.remove(transfer.id,true);
+						//	if (data.trash) torrent.remove(transfer.id,true);
 							socket.emit('alert', {'title':'Movie downloaded','message':data.movie.title});
 						});
 					}
-					if (shows.complete) {
+					if (shows.complete){
 						shows.complete(transfer).then(function(data){
-							if (data.trash) torrent.remove(transfer.id,true);
+						//	if (data.trash) torrent.remove(transfer.id,true);
 							socket.emit('alert', {'title':'Episode downloaded','message':data.show.name});
 						});
 					}
