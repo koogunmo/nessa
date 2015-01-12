196fc90125afda90ff2cbd644dc57f810eadeb61
Martin Purcell <martin.purcell@gmail.com>
2015-01-12 09:52:15 +0000

    Re-enabled DynDNS

--- server/tasks/dyndns.js
+++ server/tasks/dyndns.js
@@ -1,19 +1,29 @@
 'use strict';
 
+var log4js = require('log4js');
+log4js.configure({
+	appenders: [{
+		type: 'console'
+	}],
+	replaceConsole: true
+});
+var logger = log4js.getLogger('task-dyndns');
+
 module.exports = function(app,db,socket){
 	try {
-		/*
 		var request = require('request'), schedule = require('node-schedule');
 		var dynamicDNS = function(){
-			if (!nconf.get('system:dyndns')) return;
-			
-			return;
-			
+			if (!nconf.get('dyndns:enabled')) return;
+			logger.debug('Updating DynDNS...');
 			request('http://myip.dnsomatic.com', function(error, response, ip){
 				if (error) logger.error(error);
 				if (ip){
-					request('http://dns.silicomedia.com:8053/api/'+nconf.get('trakt:username')+'/'+ip, function(error, response, json){
-						if (error) console.error('Dynamic DNS Error:', error.message);
+					request('http://dns.silico.media:8053/api/'+nconf.get('dyndns:username')+'/'+ip, function(error, response, json){
+						if (error) logger.error(error.message);
+						if (json){
+							if (json.url) logger.debug('Public URL:', json.url);
+							logger.debug(json);
+						}
 					});
 				}
 			});
@@ -21,7 +31,6 @@ module.exports = function(app,db,socket){
 		var rule = new schedule.RecurrenceRule(); rule.minute = [0];
 		schedule.scheduleJob(rule, dynamicDNS);
 		dynamicDNS();
-		*/
 	} catch(e){
 		logger.error(e.message);
 	}
