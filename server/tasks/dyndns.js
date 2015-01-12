0c8de9647379d2729886786e86a7a87500771e5a
Martin Purcell <martin.purcell@gmail.com>
2015-01-12 12:27:40 +0000

    Improved DynDNS system

--- server/tasks/dyndns.js
+++ server/tasks/dyndns.js
@@ -15,14 +15,17 @@ module.exports = function(app,db,socket){
 		var dynamicDNS = function(){
 			if (!nconf.get('dyndns:enabled')) return;
 			logger.debug('Updating DynDNS...');
-			request('http://myip.dnsomatic.com', function(error, response, ip){
+			request.get('http://myip.dnsomatic.com', function(error, response, ip){
 				if (error) logger.error(error);
 				if (ip){
-					request('http://dns.silico.media:8053/api/'+nconf.get('dyndns:username')+'/'+ip, function(error, response, json){
-						if (error) logger.error(error.message);
+					var payload = {'address':ip,'username':nconf.get('dyndns:username')};
+					request.post('http://dns.silico.media:8053/api/update', {'body':payload,'json':true}, function(error,res,json){
+						if (error) logger.error(error);
 						if (json){
-							if (json.url) logger.debug('Public URL:', json.url);
-							logger.debug(json);
+							if (json.error) logger.error('DynDNS update error', json.error);
+							if (json.status){
+								logger.debug('DynDNS update successful');
+							}
 						}
 					});
 				}
