08c2e9fef9fec403cad55fe59bc62f486182dd00
Martin Purcell <martin.purcell@gmail.com>
2015-03-31 00:04:16 +0100

    Error loop

--- server/tasks/latest.js
+++ server/tasks/latest.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var log4js = require('log4js');
+log4js.configure({
+	appenders: [{
+		type: 'console'
+	}],
+	replaceConsole: true
+});
+var logger = log4js.getLogger('task-latest');
+
+
 /* Check for new download availability */
 module.exports = function(app,db,socket){
 	try {
